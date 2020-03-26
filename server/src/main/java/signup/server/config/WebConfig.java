4e08c3443acc9fffc7840f3a3ff3d7bc013a8f61
kyungrae <rlarudfo93@gmail.com>
2020-03-26 20:46:54 +0900

    feat: post method 추가

--- server/src/main/java/signup/server/config/WebConfig.java
+++ server/src/main/java/signup/server/config/WebConfig.java
@@ -2,6 +2,7 @@ package signup.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("*");
+                        .allowedOrigins("*")
+                        .allowedMethods(HttpMethod.POST.name());
             }
         };
     }
