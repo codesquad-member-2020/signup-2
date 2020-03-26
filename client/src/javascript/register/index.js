3afee7061e7ddd7bddb014c92b3e503c3112caed
sangwon21 <junothyme@gmail.com>
2020-03-27 03:16:50 +0900

    [#19] feat: 관심사 입력 기능 작성
    why: 관심사를 사용자에게서 입력받기 위하여 관심사를 받는 기능을
    작성했다.

--- client/src/javascript/register/index.js
+++ client/src/javascript/register/index.js
@@ -81,3 +81,86 @@ SELECT_ELEMENT(`#${REGISTER.MODAL_BUTTON}`).addEventListener(
 );
 
 SELECT_ELEMENT(`form`).addEventListener("submit", composedCheckRequirements);
+
+const tagContainer = document.querySelector(".tag-container");
+const input = document.querySelector(".tag-container input");
+
+let tags = [];
+
+function createTag(label) {
+  const div = document.createElement("div");
+  div.setAttribute("class", "tag");
+  const span = document.createElement("span");
+  span.innerHTML = label;
+  const closeIcon = document.createElement("i");
+  closeIcon.innerHTML = "close";
+  closeIcon.setAttribute("class", "material-icons");
+  closeIcon.setAttribute("data-item", label);
+  div.appendChild(span);
+  div.appendChild(closeIcon);
+  return div;
+}
+
+function clearTags() {
+  document.querySelectorAll(".tag").forEach(tag => {
+    tag.parentElement.removeChild(tag);
+  });
+}
+
+function addTags() {
+  clearTags();
+  tags
+    .slice()
+    .reverse()
+    .forEach(tag => {
+      tagContainer.prepend(createTag(tag));
+    });
+}
+
+const isValidTagNumber = () => {
+  const targetClassList = SELECT_ELEMENT("#interests-warning-message")
+    .classList;
+  if (tags.length >= 3) {
+    if (!targetClassList.contains("hide")) {
+      targetClassList.add("hide");
+    }
+  } else {
+    if (targetClassList.contains("hide")) targetClassList.remove("hide");
+  }
+};
+
+input.addEventListener("keyup", e => {
+  if (e.key === ",") {
+    const { value } = e.target;
+    const withoutComma = value.slice(0, value.length - 1);
+    [withoutComma].forEach(tag => {
+      tags.push(tag);
+    });
+
+    addTags();
+    input.value = "";
+    isValidTagNumber();
+    return;
+  }
+  if (e.key === "Backspace") {
+    clearTags();
+    tags.pop();
+    tags
+      .slice()
+      .reverse()
+      .forEach(tag => {
+        tagContainer.prepend(createTag(tag));
+      });
+    isValidTagNumber();
+  }
+});
+document.addEventListener("click", e => {
+  if (e.target.tagName === "I") {
+    const tagLabel = e.target.getAttribute("data-item");
+    const index = tags.indexOf(tagLabel);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    addTags();
+  }
+});
+
+input.focus();
