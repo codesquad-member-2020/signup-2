9ad023564e1ec9b5078fc57f04928597a1879ca1
corykim0829 <corykim0829@gmail.com>
2020-03-27 01:37:28 +0900

    [#2] Feat: User 모델

---
+++ mobile/SignUpApp/SignUpApp/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  SignUpApp
+//
+//  Created by Cory Kim on 2020/03/27.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    var identification: String
+    var password: String
+    var name: String
+    var birthDate: Date
+    var email: String
+    var phone: String
+}
