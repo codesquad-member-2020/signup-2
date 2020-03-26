c754c00952487f163777388e422ed8b770e6fc54
kyungrae <rlarudfo93@gmail.com>
2020-03-26 17:37:01 +0900

    feat: 테스트를 위해 cors 설정

--- server/src/main/java/signup/server/config/WebConfig.java
+++ server/src/main/java/signup/server/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/message/**")
+                registry.addMapping("/api/**")
                         .allowedOrigins("*");
             }
         };
