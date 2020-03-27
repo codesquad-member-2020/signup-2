1d486af088c27470c79e874ff36a4b545ba95c9d
corykim0829 <corykim0829@gmail.com>
2020-03-28 02:13:37 +0900

    [#31] Feat: Action Sheet 구조 및 titleColor 변경

--- mobile/SignUpApp/SignUpApp/Controllers/TermsViewController.swift
+++ mobile/SignUpApp/SignUpApp/Controllers/TermsViewController.swift
@@ -11,23 +11,39 @@ import UIKit
 class TermsViewController: UIViewController {
     
     @IBOutlet weak var termTextView: UITextView!
+    
+    let actionSheetController: UIAlertController = {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        return actionSheet
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         setupUI()
+        setupActionSheetHandlers()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "동의", style: .default, handler: { (_) in
-            self.dismiss(animated: true, completion: nil)
-        }))
-        actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { (_) in
-            self.dismiss(animated: true, completion: nil)
-        }))
+    private func setupActionSheetHandlers() {
+        let agreeAction = UIAlertAction(title: "동의", style: .default) { (_) in
+            self.didSelectAction(hasAgreed: true)
+        }
+        agreeAction.setValue(UIColor(named: "KeyColor"), forKey: "titleTextColor")
         
-        self.present(actionSheet, animated: true, completion: nil)
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { (_) in
+            self.didSelectAction(hasAgreed: false)
+        })
+        actionSheetController.addAction(agreeAction)
+        actionSheetController.addAction(cancelAction)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        present(actionSheetController, animated: true, completion: nil)
+    }
+    
+    private func didSelectAction(hasAgreed: Bool) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     private func setupUI() {
