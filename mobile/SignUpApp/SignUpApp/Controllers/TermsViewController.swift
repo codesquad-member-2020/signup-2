ca6be38f5261b905be6940ab23b6bde0eb1f5ecf
corykim0829 <corykim0829@gmail.com>
2020-03-27 16:44:59 +0900

    [#31] Feat: Present action sheet

--- mobile/SignUpApp/SignUpApp/Controllers/TermsViewController.swift
+++ mobile/SignUpApp/SignUpApp/Controllers/TermsViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class TermsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "동의", style: .default, handler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        actionSheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { (_) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        self.present(actionSheet, animated: true, completion: nil)
     }
-    */
-
 }
