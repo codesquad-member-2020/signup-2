b6dee51c629c10feae8a209e6b4c7aa54f232b13
kyungrae <rlarudfo93@gmail.com>
2020-03-27 12:13:43 +0900

    fix: 포트 수정

--- server/src/main/java/signup/server/config/WebConfig.java
+++ server/src/main/java/signup/server/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://127.0.0.1:5050","http://localhost:5050")
+                        .allowedOrigins("http://127.0.0.1:5500","http://localhost:5500")
                         .allowedMethods("GET","POST")
                         .allowCredentials(true).maxAge(3600);
             }
