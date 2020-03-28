aecc84ef1da361d1668e2c359966b6f66940b3db
corykim0829 <corykim0829@gmail.com>
2020-03-28 16:45:19 +0900

    [#42] Feat: 관심사 Cell 커스터마이징

---
+++ mobile/SignUpApp/SignUpApp/Controllers/InterestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InterestViewController.swift
+//  SignUpApp
+//
+//  Created by Cory Kim on 2020/03/28.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class InterestViewController:  UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
