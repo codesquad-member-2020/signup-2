60e5cfb2266c84e588bb8add571872188de18f57
corykim0829 <corykim0829@gmail.com>
2020-03-28 16:58:53 +0900

    [#42] Feat: Interest Cell estimatedSize

--- mobile/SignUpApp/SignUpApp/Controllers/InterestViewController.swift
+++ mobile/SignUpApp/SignUpApp/Controllers/InterestViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class InterestViewController:  UIViewController {
+class InterestViewController:  UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var interestsItem: [String] = ["동준", "안녕하세요", "춤"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InterestCell", for: indexPath) as! InterestCell
+        cell.interest = interestsItem[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let estimatedSizeCell = InterestCell(frame: .init(x: 0, y: 0, width: 160, height: 40))
+        estimatedSizeCell.interest = interestsItem[indexPath.item]
+        estimatedSizeCell.layoutIfNeeded()
+        let estimatedSize = estimatedSizeCell.systemLayoutSizeFitting(.init(width: 160, height: 40))
+        return .init(width: estimatedSize.width, height: 40)
     }
 }
